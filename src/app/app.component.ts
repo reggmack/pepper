3574e0b9b15eb0bd3df1746d7941d355f19616f2
Reggie Mack <thegraphixninja@gmail.com>
2017-02-25 16:00:10 -0600

    Implement Facebook Login
    Set up Facebook app, Add Product, OAuth Firebase

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit, OnDestroy } from '@angular/core';
-import { AngularFire, FirebaseListObservable } from 'angularfire2';
+import { AngularFire, FirebaseListObservable, AuthProviders, AuthMethods } from 'angularfire2';
 import { Observable } from 'rxjs/Observable';
 import 'rxjs/add/operator/map';
 import 'rxjs/add/operator/take';
@@ -9,32 +9,23 @@ import 'rxjs/add/operator/take';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-    title = 'The Pepperiest!';
-    cuisines: FirebaseListObservable<any[]>;
-    restaurants: Observable<any[]>;
-    exists;
-  
-    constructor(private af: AngularFire){ 
+export class AppComponent implements OnInit {
+    constructor(private af: AngularFire){    
     }
-    
+
     ngOnInit() {
-        // /restaurants
-        // /restaurants-by-city/camberwell
-
-        this.af.database.list('/restaurants').push({ name: '' })
-            .then(x => {
-                // x.key
-
-                let restaurant = { name: 'My New Restaurant' };
-
-                let update = {};
-                //update.firstName = '';
-                update['restaurants/' + x.key] = { name: 'My New Restaurant' };
-                update['restaurants-by-city/camberwell' + x.key] = { name: 'My New Restaurant' };
-
-                this.af.database.object('/').update(update);
-            });
     }
-        
+
+    login() {    
+        this.af.auth.login({
+            provider: AuthProviders.Facebook,
+            method:AuthMethods.Popup
+        }).then(authState => {
+            console.log("AFTER LOGIN", authState);
+        });
+    }
+
+    logout() {
+        this.af.auth.logout();      
+    }        
 }
